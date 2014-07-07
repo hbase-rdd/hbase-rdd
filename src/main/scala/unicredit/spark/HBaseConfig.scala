ce5952efd5f1b3700ea3a7dd685955de39a77afe
Andrea Ferretti <ferrettiandrea@gmail.com>
2014-07-07 10:32:36 +0200

    Added some comments

--- src/main/scala/unicredit/spark/HBaseConfig.scala
+++ src/main/scala/unicredit/spark/HBaseConfig.scala
@@ -5,10 +5,29 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.spark.rdd.RDD
 
 
+/**
+ * Wrapper for HBaseConfiguration
+ */
 class HBaseConfig(defaults: Configuration) extends Serializable {
   def get = HBaseConfiguration.create(defaults)
 }
 
+/**
+ * Factories to generate HBaseConfig instances.
+ *
+ * We can generate an HBaseConfig
+ * - from an existing HBaseConfiguration
+ * - from a sequence of String key/value pairs
+ * - from an existing object with a `rootdir` and `quorum` members
+ *
+ * The two latter cases are provided for simplicity
+ * (ideally a client should not have to deal with the native
+ * HBase API). In these cases, we also add two default values.
+ *
+ * The last constructor contains the minimum information to
+ * be able to read and write to the HBase cluster. It can be used
+ * in tandem with a case class containing job configuration.
+ */
 object HBaseConfig {
   def apply(conf: Configuration): HBaseConfig = new HBaseConfig(conf)
 
