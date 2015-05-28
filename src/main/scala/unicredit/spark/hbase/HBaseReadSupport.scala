52ad0ffe35adf8da9c2a3d8585229e422ca1b058
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2015-05-28 18:50:09 +0200

    improved implementation, updated readme

--- src/main/scala/unicredit/spark/hbase/HBaseReadSupport.scala
+++ src/main/scala/unicredit/spark/hbase/HBaseReadSupport.scala
@@ -17,7 +17,7 @@ package unicredit.spark.hbase
 
 import scala.collection.JavaConversions._
 
-import org.apache.hadoop.hbase.CellUtil
+import org.apache.hadoop.hbase.{ Cell, CellUtil }
 import org.apache.hadoop.hbase.client.{ Result, Scan }
 import org.apache.hadoop.hbase.mapreduce.{ TableInputFormat, IdentityTableMapper, TableMapReduceUtil }
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
@@ -51,26 +51,37 @@ trait HBaseReadSupport {
 }
 
 final class HBaseSC(@transient sc: SparkContext) extends Serializable {
-  private def extract(data: Map[String, Set[String]], result: Result) =
+  private def extract[A, B](data: Map[String, Set[String]], result: Result, read: Cell => B) =
     data map {
       case (cf, columns) =>
         val content = columns map { column =>
           val cell = result.getColumnLatestCell(cf.getBytes, column.getBytes)
-          column -> cell
+          column -> read(cell)
         } toMap
 
         cf -> content
     }
 
-  private def extractRow(data: Set[String], result: Result) =
+  private def extractRow[A, B](data: Set[String], result: Result, read: Cell => B) =
     result.listCells groupBy { cell =>
       new String(CellUtil.cloneFamily(cell))
     } filterKeys data.contains mapValues { cells =>
       cells map { cell =>
         val column = new String(CellUtil.cloneQualifier(cell))
-        column -> cell
+        column -> read(cell)
       } toMap
     }
+
+  private def read[A](cell: Cell)(implicit reader: Reads[A]) = {
+    val value = CellUtil.cloneValue(cell)
+    reader.read(value)
+  }
+
+  private def readTS[A](cell: Cell)(implicit reader: Reads[A]) = {
+    val value = CellUtil.cloneValue(cell)
+    val timestamp = cell.getTimestamp
+    (reader.read(value), timestamp)
+  }
 
   private def makeConf(config: HBaseConfig, table: String, columns: Option[String] = None) = {
     val conf = config.get
@@ -98,21 +109,24 @@ final class HBaseSC(@transient sc: SparkContext) extends Serializable {
   def hbase[A](table: String, data: Map[String, Set[String]])(implicit config: HBaseConfig, reader: Reads[A]) = {
     hbaseRaw(table, data) map {
       case (key, row) =>
-        Bytes.toString(key.get) -> extract(data, row).mapValues(_.mapValues { cell =>
-          val value = CellUtil.cloneValue(cell)
-          reader.read(value)
-        })
+        Bytes.toString(key.get) -> extract(data, row, read[A])
     }
   }
 
+  /**
+   * Provides an RDD of HBase rows. Here `data` is a map whose
+   * keys represent the column families and whose values are
+   * the list of columns to be read from the family.
+   *
+   * Returns an `RDD[(String, Map[String, Map[String, (A, Long)]])]`, where
+   * the first element is the rowkey and the second element is a
+   * nested map which associates column family and column to
+   * the tuple (value, timestamp). Columns which are not found are omitted from the map.
+   */
   def hbaseTS[A](table: String, data: Map[String, Set[String]])(implicit config: HBaseConfig, reader: Reads[A]) = {
     hbaseRaw(table, data) map {
       case (key, row) =>
-        Bytes.toString(key.get) -> extract(data, row).mapValues(_.mapValues { cell =>
-          val value = CellUtil.cloneValue(cell)
-          val timestamp = cell.getTimestamp
-          (reader.read(value), timestamp)
-        })
+        Bytes.toString(key.get) -> extract(data, row, readTS[A])
     }
   }
 
@@ -138,21 +152,23 @@ final class HBaseSC(@transient sc: SparkContext) extends Serializable {
   def hbase[A](table: String, data: Set[String])(implicit config: HBaseConfig, reader: Reads[A]) = {
     hbaseRaw(table, data) map {
       case (key, row) =>
-        Bytes.toString(key.get) -> extractRow(data, row).mapValues(_.mapValues { cell =>
-          val value = CellUtil.cloneValue(cell)
-          reader.read(value)
-        })
+        Bytes.toString(key.get) -> extractRow(data, row, read[A])
     }
   }
 
+  /**
+   * Provides an RDD of HBase rows. Here `data` is a set of
+   * colum families, which are read in full.
+   *
+   * Returns an `RDD[(String, Map[String, Map[String, (A, Long)]])]`, where
+   * the first element is the rowkey and the second element is a
+   * nested map which associated column family and column to
+   * the tuple (value, timestamp).
+   */
   def hbaseTS[A](table: String, data: Set[String])(implicit config: HBaseConfig, reader: Reads[A]) = {
     hbaseRaw(table, data) map {
       case (key, row) =>
-        Bytes.toString(key.get) -> extractRow(data, row).mapValues(_.mapValues { cell =>
-          val value = CellUtil.cloneValue(cell)
-          val timestamp = cell.getTimestamp
-          (reader.read(value), timestamp)
-        })
+        Bytes.toString(key.get) -> extractRow(data, row, readTS[A])
     }
   }
 
