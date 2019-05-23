5eec3d8b2c0fad307e127128c415e65b9d95fb36
Francesco MDE <francesco.montecuccolideglierri@unicredit.eu>
2019-05-23 14:07:03 +0200

    update header license

--- src/main/scala/unicredit/spark/hbase/HFileSupport.scala
+++ src/main/scala/unicredit/spark/hbase/HFileSupport.scala
@@ -1,4 +1,4 @@
-/* Copyright 2014 UniCredit S.p.A.
+/* Copyright 2019 UniCredit S.p.A.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
